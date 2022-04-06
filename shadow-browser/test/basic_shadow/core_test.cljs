58c51e2368f456e2ec0964efdd0197ca9fc36348
Arne Brasseur <arne@arnebrasseur.net>
2022-04-06 13:55:11 +0200

    Make tests themselves a bit more interesting

--- shadow-browser/test/basic_shadow/core_test.cljs
+++ shadow-browser/test/basic_shadow/core_test.cljs
@@ -1,5 +1,26 @@
 (ns basic-shadow.core-test
   (:require [clojure.test :refer [deftest testing is]]))
 
-(deftest basic-test
-  (is (= 1 (inc 0))))
+(deftest passing-tests
+  (is (= 1 (inc 0)))
+
+  (is (pos-int? 5))
+
+  (is (thrown? cljs.core/ExceptionInfo (throw (ex-info "Oh no!" {:pos :thrown})))))
+
+(deftest failing-tests
+  (testing "These tests are regular assertion failures"
+    (is (= 1 2))
+
+    (is (= [{:x 1} {:y 2} {:z 3}]
+           [{:x 1} {:y 0} {:z 3}]))
+
+    (is (throw (ex-info "Oh no!" {:pos :is})))))
+
+(deftest forgot-is
+  (testing "Kaocha catches this as a deftest without an assertion, it's a common mistake"
+    (= 5 4)))
+
+(deftest exception-outside-assertion
+  (testing "Exceptions are reported as :error results"
+    (throw (ex-info "Oh no!" {:pos :outside}))))
